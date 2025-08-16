bdba029318df1ce9942efe4f698d7021b4af440d
srilaks1404-rgb <srilaks1404@gmail.com>
2025-08-15 21:48:54 -0500

    first commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def in_autotests_we_trust(a, b):
     if a == b:
-        print('Pass')
+        print('Test passed')
     else:
-        print('FAIL')
+        print('Test failed')
 
 in_autotests_we_trust(10, '10')
 
